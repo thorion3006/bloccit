89e020bb802d4647f348d04d26ace01a063c727c
Sajeev Ramasamy <thorion3006@gmail.com>
2017-04-01 19:11:10 +0000

    Corrections

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,6 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-    
-    @posts.each do |post|
-      post.title = "SPAM" if post.id == 1 || post.id % 5 == 0
-    end
   end
 
   def show
@@ -14,12 +10,12 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
-  
+
   def create
     @post = Post.new
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
-    
+
     if @post.save
       flash[:notice] = "Post was saved."
       redirect_to @post
