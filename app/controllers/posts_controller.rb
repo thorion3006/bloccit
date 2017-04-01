0b3334358240f95b5dee65c5532f972bee34ac18
Sajeev Ramasamy <thorion3006@gmail.com>
2017-04-01 19:00:51 +0000

    Checkpoint 19 CRUD: Completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,32 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    
+    @posts.each do |post|
+      post.title = "SPAM" if post.id == 1 || post.id % 5 == 0
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+    
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
   end
 
   def edit
