7feb0000873004f068a0a58e3b180715bbb884aa
Sajeev Ramasamy <thorion3006@gmail.com>
2017-04-05 12:26:56 +0000

    Rails server working in vagrant

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,17 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+if ENV["RAILS_ENV"] == "development" || ENV["RAILS_ENV"].nil?
+  require "rails/commands/server"
+
+  module Rails
+    class Server
+      alias_method :original_default_options, :default_options
+
+      def default_options
+        original_default_options.merge(Host: "0.0.0.0")
+      end
+    end
+  end
+end
