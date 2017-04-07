797a6f8fd8b86a6c31b20cb7ac543b02e2cba511
Sajeev Ramasamy <thorion3006@gmail.com>
2017-04-07 16:09:19 +0000

    Assignment 26 Authentication - Signing In: Completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
     css_Class << ' has-error' if errors.any?
     content_tag :div, capture(&block), class: css_Class
   end
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  end
 end
