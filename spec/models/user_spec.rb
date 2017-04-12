3fde793b9c855a522aa933ef764841324d9845cd
Sajeev Ramasamy <thorion3006@gmail.com>
2017-04-12 11:50:23 +0000

    Corrections in rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.create(:user) }
+  let(:user) { create(:user) }
 
   it { is_expected.to have_many(:posts) }
   it { is_expected.to have_many(:comments) }
